38e1f027a3ddbf01c33b0d077e3b753b56d58dfb
brendino500 <brenda.ty@live.com>
2020-11-18 13:27:20 +0000

    methods set up for Paddle

--- src/Paddle.java
+++ src/Paddle.java
@@ -8,4 +8,24 @@ public class Paddle {
   Paddle() {
 
   }
+
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  public void keyReleased(KeyEvent e) {
+
+  }
+
+  public void setYDirection(int yDirection) {
+
+  }
+
+  public void move() {
+
+  }
+
+  public void draw(Graphics g) {
+
+  }
 }
